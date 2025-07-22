bc84b25e7f4c638d380ddf6ea126cb5451a77637
Martin Hutchinson <mhutchinson@gmail.com>
2025-07-22 16:28:22 +0000

    [VIndex] Refactoring and cleaning tech debt
    The next milestone is to add the output log. While prototyping this, it became clear that a lot of the naming is confusing. Renamed to make clear what is the input log, what is the write ahead log, etc. This makes space to add the output log parameters etc afterwards.
    
    As part of this, simplified the storage flags so that it takes only a root directory and then creates the input and output logs in there, along with the WAL file. Other map persistence will likely be added to this directory too in the future.
    
    Use skey->vkey method from formats, and deleted local method.
    
    Added a high level test for the verifiable index.

--- vindex/map_test.go
+++ vindex/map_test.go
@@ -17,15 +17,29 @@
 package vindex
 
 import (
+	"bytes"
+	"context"
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
 	"io"
+	"iter"
 	"os"
 	"testing"
 	"time"
 
+	"github.com/google/go-cmp/cmp"
+	"github.com/transparency-dev/formats/log"
+	fnote "github.com/transparency-dev/formats/note"
+	"github.com/transparency-dev/merkle/rfc6962"
+	"github.com/transparency-dev/merkle/testonly"
+	"golang.org/x/mod/sumdb/note"
 	"golang.org/x/sync/errgroup"
+)
+
+const (
+	skey = "PRIVATE+KEY+logandmap+38581672+AXJ0FKWOcO2ch6WC8kP705Ed3Gxu7pVtZLhfHAQwp+FE"
+	vkey = "logandmap+38581672+Ab/PCr1eCclRPRMBqw/r5An1xO71MCnImLiospEq6b4l"
 )
 
 func TestWriteAheadLog_init(t *testing.T) {
@@ -81,7 +95,7 @@ func TestWriteAheadLog_init(t *testing.T) {
 			if err := f.Close(); err != nil {
 				t.Fatal(err)
 			}
-			wal := &writeAheadLog{
+			wal := &walWriter{
 				walPath: f.Name(),
 			}
 			idx, err := wal.init()
@@ -113,7 +127,7 @@ func TestWriteAheadLog_roundtrip(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	wal := &writeAheadLog{
+	wal := &walWriter{
 		walPath: f.Name(),
 	}
 	idx, err := wal.init()
@@ -160,7 +174,7 @@ func TestWriteAndWriteLog(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	wal := &writeAheadLog{
+	wal := &walWriter{
 		walPath: f.Name(),
 	}
 	idx, err := wal.init()
@@ -171,7 +185,7 @@ func TestWriteAndWriteLog(t *testing.T) {
 		t.Fatalf("expected index %d, got %d", want, got)
 	}
 
-	reader, err := newLogReader(f.Name())
+	reader, err := newWalReader(f.Name())
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -261,6 +275,108 @@ func TestUnmarshal(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestVerifiableIndex(t *testing.T) {
+	ctx := context.Background()
+	s, v, err := fnote.NewEd25519SignerVerifier(skey)
+	if err != nil {
+		t.Fatal(err)
+	}
+	inputLog := &inMemoryTreeSource{
+		t:      testonly.New(rfc6962.DefaultHasher),
+		leaves: make([][]byte, 0),
+		s:      s,
+	}
+	for _, str := range []string{"foo: 2", "bar: 5", "bar: 10", "foo: 8"} {
+		inputLog.Append(str)
+	}
+
+	inputLogCpParseFn := func(cpRaw []byte) (*log.Checkpoint, error) {
+		cp, _, _, err := log.ParseCheckpoint(cpRaw, v.Name(), v)
+		return cp, err
+	}
+	mapFn := func(leaf []byte) [][32]byte {
+		key, _, found := bytes.Cut(leaf, []byte(":"))
+		if !found {
+			panic("colon not found")
+		}
+		return [][32]byte{sha256.Sum256(key)}
+	}
+	f, err := os.CreateTemp("", "testWal")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := f.Close(); err != nil {
+		t.Fatal(err)
+	}
+	vi, err := NewVerifiableIndex(ctx, inputLog, inputLogCpParseFn, mapFn, f.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := vi.Update(ctx); err != nil {
+		t.Fatal(err)
+	}
+
+	idxes, size := vi.Lookup(sha256.Sum256([]byte("foo")))
+	if size != 4 {
+		t.Errorf("expected size 4 but got %d", size)
+	}
+	if want := []uint64{0, 3}; !cmp.Equal(idxes, want) {
+		t.Errorf("expected %v but got %v", want, idxes)
+	}
+
+	idxes, size = vi.Lookup(sha256.Sum256([]byte("bar")))
+	if size != 4 {
+		t.Errorf("expected size 4 but got %d", size)
+	}
+	if want := []uint64{1, 2}; !cmp.Equal(idxes, want) {
+		t.Errorf("expected %v but got %v", want, idxes)
+	}
+
+	idxes, size = vi.Lookup(sha256.Sum256([]byte("banana")))
+	if size != 4 {
+		t.Errorf("expected size 4 but got %d", size)
+	}
+	if idxes != nil {
+		t.Errorf("expected no results but got %+v", idxes)
+	}
+}
+
+type inMemoryTreeSource struct {
+	t      *testonly.Tree
+	leaves [][]byte
+	s      note.Signer
+}
+
+func (s *inMemoryTreeSource) Checkpoint(ctx context.Context) (checkpoint []byte, err error) {
+	rootHash := s.t.Hash()
+	size := uint64(len(s.leaves))
+
+	cp := log.Checkpoint{
+		Origin: s.s.Name(),
+		Size:   size,
+		Hash:   rootHash,
+	}
+	n := &note.Note{Text: string(cp.Marshal())}
+	return note.Sign(n, s.s)
+}
+
+func (s *inMemoryTreeSource) Leaves(ctx context.Context, start, end uint64) iter.Seq2[[]byte, error] {
+	return func(yield func([]byte, error) bool) {
+		for _, entry := range s.leaves {
+			if !yield(entry, nil) {
+				return
+			}
+		}
+	}
+}
+
+func (s *inMemoryTreeSource) Append(leafStr string) {
+	leaf := []byte(leafStr)
+	s.leaves = append(s.leaves, leaf)
+	s.t.Append(leaf)
 }
 
 func mustHashEncode(data string) string {
