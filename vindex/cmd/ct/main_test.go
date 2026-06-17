7cf9ea023701ac7e74eb59798e352c689fc8cd79
Martin Hutchinson <mhutchinson@gmail.com>
2026-06-17 13:59:01 +0000

    Support pre-certs

--- vindex/cmd/ct/main_test.go
+++ vindex/cmd/ct/main_test.go
@@ -33,7 +33,7 @@ import (
 	"golang.org/x/crypto/cryptobyte"
 )
 
-func createTestCertBytes(dnsNames []string) ([]byte, error) {
+func createTestCertBytes(dnsNames []string, isPrecert bool) ([]byte, error) {
 	priv, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
 		return nil, err
@@ -55,52 +55,126 @@ func createTestCertBytes(dnsNames []string) ([]byte, error) {
 		return nil, err
 	}
 
+	var tbsDER []byte
+	if isPrecert {
+		parsed, err := x509.ParseCertificate(certDER)
+		if err != nil {
+			return nil, err
+		}
+		tbsDER = parsed.RawTBSCertificate
+	}
+
 	// Now marshal using cryptobyte into the form expected by mapFn
-	// version = 0, leafType = 0, timestamp = 12345678, certType = 0 (x509)
+	// version = 0, leafType = 0, timestamp = 12345678
 	var b cryptobyte.Builder
 	b.AddUint8(0)         // version
 	b.AddUint8(0)         // leafType
 	b.AddUint64(12345678) // timestamp
-	b.AddUint16(0)        // certType (x509)
-	b.AddUint24LengthPrefixed(func(b *cryptobyte.Builder) {
-		b.AddBytes(certDER)
-	})
+	if isPrecert {
+		b.AddUint16(1) // certType (precert)
+		dummyIKH := make([]byte, 32)
+		b.AddBytes(dummyIKH)
+		b.AddUint24LengthPrefixed(func(b *cryptobyte.Builder) {
+			b.AddBytes(tbsDER)
+		})
+	} else {
+		b.AddUint16(0) // certType (x509)
+		b.AddUint24LengthPrefixed(func(b *cryptobyte.Builder) {
+			b.AddBytes(certDER)
+		})
+	}
 	return b.Bytes()
 }
 
 func TestMapFn(t *testing.T) {
 	testCases := []struct {
-		desc     string
-		dnsNames []string
-		wantKeys []string
+		desc      string
+		dnsNames  []string
+		wantKeys  []string
+		isPrecert bool
+		rawBytes  []byte
 	}{
 		{
-			desc:     "standard domains and wildcards",
-			dnsNames: []string{"*.google.com", "google.com", "maps.google.com"},
-			wantKeys: []string{"google.com", "maps.google.com"},
+			desc:      "standard domains and wildcards",
+			dnsNames:  []string{"*.google.com", "google.com", "maps.google.com"},
+			wantKeys:  []string{"google.com", "maps.google.com"},
+			isPrecert: false,
 		},
 		{
-			desc:     "deeper subdomain",
-			dnsNames: []string{"deep.maps.google.co.uk"},
-			wantKeys: []string{"deep.maps.google.co.uk", "maps.google.co.uk", "google.co.uk"},
+			desc:      "deeper subdomain",
+			dnsNames:  []string{"deep.maps.google.co.uk"},
+			wantKeys:  []string{"deep.maps.google.co.uk", "maps.google.co.uk", "google.co.uk"},
+			isPrecert: false,
 		},
 		{
-			desc:     "mixed case",
-			dnsNames: []string{"MAPS.GOOGLE.COM"},
-			wantKeys: []string{"maps.google.com", "google.com"},
+			desc:      "mixed case",
+			dnsNames:  []string{"MAPS.GOOGLE.COM"},
+			wantKeys:  []string{"maps.google.com", "google.com"},
+			isPrecert: false,
 		},
 		{
-			desc:     "invalid or TLD",
-			dnsNames: []string{"localhost", "*.co.uk"},
-			wantKeys: []string{"localhost", "co.uk"},
+			desc:      "invalid or TLD",
+			dnsNames:  []string{"localhost", "*.co.uk"},
+			wantKeys:  []string{"localhost", "co.uk"},
+			isPrecert: false,
+		},
+		{
+			desc:      "precert: standard domains and wildcards",
+			dnsNames:  []string{"*.google.com", "google.com", "maps.google.com"},
+			wantKeys:  []string{"google.com", "maps.google.com"},
+			isPrecert: true,
+		},
+		{
+			desc:      "precert: deeper subdomain",
+			dnsNames:  []string{"deep.maps.google.co.uk"},
+			wantKeys:  []string{"deep.maps.google.co.uk", "maps.google.co.uk", "google.co.uk"},
+			isPrecert: true,
+		},
+		{
+			desc:     "truncated header",
+			rawBytes: []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // 11 bytes, header is 12 bytes
+			wantKeys: []string{},
+		},
+		{
+			desc: "unknown cert type",
+			rawBytes: []byte{
+				0,                      // version
+				0,                      // leafType
+				0, 0, 0, 0, 0, 0, 0, 0, // timestamp
+				0, 99, // certType = 99
+			},
+			wantKeys: []string{},
+		},
+		{
+			desc: "precert truncated TBS",
+			rawBytes: func() []byte {
+				var b cryptobyte.Builder
+				b.AddUint8(0)         // version
+				b.AddUint8(0)         // leafType
+				b.AddUint64(12345678) // timestamp
+				b.AddUint16(1)        // certType (precert)
+				dummyIKH := make([]byte, 32)
+				b.AddBytes(dummyIKH)
+				b.AddUint24(100)                  // length prefix 100
+				b.AddBytes([]byte{1, 2, 3, 4, 5}) // only 5 bytes of data
+				bytes, _ := b.Bytes()
+				return bytes
+			}(),
+			wantKeys: []string{},
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.desc, func(t *testing.T) {
-			data, err := createTestCertBytes(tc.dnsNames)
-			if err != nil {
-				t.Fatalf("failed to create test cert: %v", err)
+			var data []byte
+			var err error
+			if tc.rawBytes != nil {
+				data = tc.rawBytes
+			} else {
+				data, err = createTestCertBytes(tc.dnsNames, tc.isPrecert)
+				if err != nil {
+					t.Fatalf("failed to create test cert: %v", err)
+				}
 			}
 			hashes := mapFn(data)
 
@@ -146,4 +220,3 @@ func TestMetricsEndpoint(t *testing.T) {
 		t.Errorf("GET /metrics: got status %d, want %d", w.Code, http.StatusOK)
 	}
 }
-
